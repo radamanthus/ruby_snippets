80ab6fe0ecf7abd9e878f6c16d747a46de2d69de
Rad Batnag <radamanthus@gmail.com>
2015-08-30 12:03:33 +0800

    Parameterize rename_files.rb

--- rename_files.rb
+++ rename_files.rb
@@ -1,5 +1,8 @@
-files = Dir.glob('*png')
+prefix = ARGV[0]
+suffix = ARGV[1]
+string_to_remove = ARGV[2]
+files = Dir.glob("#{prefix}*#{suffix}")
 files.each do |old_filename|
-  new_filename = old_filename.gsub(' [www.imagesplitter.net]-0', '')
+  new_filename = old_filename.gsub(string_to_remove, '')
   File.rename(old_filename, new_filename)
 end
